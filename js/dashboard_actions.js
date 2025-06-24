31a7355fc3a0be53d5cb7c5468bbd89a4f06de4b
HBingXian <derp.hbx@gmail.com>
2025-06-24 15:42:24 +0800

    Updated Key Generation, Added random key generation, exporting/downloading key (json), removed key recovery

--- js/dashboard_actions.js
+++ js/dashboard_actions.js
@@ -1,18 +1,13 @@
 const { ipcRenderer } = require('electron');
 
-// Logout functionality
+// Logout
 document.getElementById('logoutBtn').addEventListener('click', () => {
     ipcRenderer.send('logout-request');
 });
 
-// Navigate to gen_key.html 
+// Navigate to key generation
 document.getElementById('generateKeyBtn').addEventListener('click', () => {
     ipcRenderer.send('navigate-to-gen-key');
-});
-
-// Navigate to rec_key.html 
-document.getElementById('recoverKeyBtn').addEventListener('click', () => {
-    ipcRenderer.send('navigate-to-rec-key');
 });
 
 // Encrypt & Upload
@@ -25,25 +20,17 @@ document.getElementById('downloadDecryptBtn').addEventListener('click', () => {
     ipcRenderer.send('request-download-decrypt');
 });
 
-
-document.getElementById('encryptUploadBtn').addEventListener('click', () => {
-    ipcRenderer.send('navigate-to-encrypt-page');
-});
-
-//navigate to encryption page
+// Navigate to manual encryption page
 document.getElementById('goToEncryptPageBtn').addEventListener('click', () => {
     ipcRenderer.send('navigate-to-encrypt-page');
 });
 
-//navigate to decryption page
+// Navigate to manual decryption page
 document.getElementById('goToDecryptPageBtn').addEventListener('click', () => {
     ipcRenderer.send('navigate-to-decrypt-page');
 });
 
-//navigate to files.html page
+// Navigate to manage files
 document.getElementById('manageFilesBtn').addEventListener('click', () => {
-  ipcRenderer.send('navigate-to-files');  // custom IPC event
+    ipcRenderer.send('navigate-to-files');
 });
-
-
-
